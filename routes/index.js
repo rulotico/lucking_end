2ba90ea6d15a48e7668399f9b02493834dd11500
Rulótico_mcn <rulotico@gmail.com>
2017-08-03 05:35:22 -0500

    update:places

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var register = require("../controller/register.js");
+var places = require('../controller/places.js');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,5 +21,15 @@ router.get('/registro', function(req, res, next) {
 });
 
 
+
+router.get('/places', function(req, res, next) {
+
+  categories=req.query.categories;
+  cost=req.query.cost;
+  distance=req.query.distance;
+
+  places.places("29.941082","-90.078772",categories,cost,distance,req,res);
+
+});
 
 module.exports = router;
